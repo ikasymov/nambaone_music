92325f08ede8c21afedd2e16a59722dc5ba851f6
ilgizkasymov <ilgizkasymov@gmail.com>
2017-10-12 13:00:11 +0600

    fix bag

--- routes/index.js
+++ routes/index.js
@@ -154,7 +154,8 @@ router.post('/', async function(req, res, next){
         return res.end()
       }
       await sendMessage(chat_id, 'Это может занять от 5 секунды до 1 минуты в зависимости от вашего скорости интернета')
-      await sendFile(downUrl, user.sender_id, chat_id)
+      res.end();
+      await sendFile(downUrl, user[0].sender_id, chat_id)
     }
     let text = await searchMusics(content, user[0]);
     await sendMessage(chat_id, text);
